cb8887564cce103cadfe3a7ffd40e66b8fa00411
Azizul Islam Rahad <65822873+gazirahad7@users.noreply.github.com>
2024-09-24 16:55:06 +0600

    upload source code

---
+++ app/about/page.js
@@ -0,0 +1,21 @@
+function page() {
+  return (
+    <div className="container mx-auto py-20">
+      <h1 className="text-4xl py-5">This is about page </h1>
+
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the standard dummy text ever since the
+        1500s, when an unknown printer took a galley of type and scrambled it to
+        make a type specimen book. It has survived not only five centuries, but
+        also the leap into electronic typesetting, remaining essentially
+        unchanged. It was popularised in the 1960s with the release of Letraset
+        sheets containing Lorem Ipsum passages, and more recently with desktop
+        publishing software like Aldus PageMaker including versions of Lorem
+        Ipsum.
+      </p>
+    </div>
+  );
+}
+
+export default page;
--- app/page.js
+++ app/page.js
@@ -1,9 +1,18 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <h1>Next App</h1>
+
+        <button
+          type="button"
+          class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+        >
+          <Link href="/about">About</Link>
+        </button>
         <Image
           className="dark:invert"
           src="https://nextjs.org/icons/next.svg"
